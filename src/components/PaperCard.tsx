8242cb9deda26bc7b6848077ec4c08d8741e1184
Isaiah Moore <imoore@udel.edu>
2026-05-23 18:08:09 -0400

    PaperCard.tsx | Wrote Authors component.

--- src/components/PaperCard.tsx
+++ src/components/PaperCard.tsx
@@ -1,4 +1,5 @@
 import { Paper } from "../types/Paper"
+import { samplePapers } from "./PaperList";
 
 interface PaperCardProps {
     paper: Paper;
@@ -8,7 +9,10 @@ export function PaperCard({paper}: PaperCardProps) {
     return (
         <div>
             <h2>{paper.title}</h2>
+
+            <p>
+                <strong>Authors:</strong> {paper.authors.join(", ")}
+            </p>
         </div>
-        
     )
 }
