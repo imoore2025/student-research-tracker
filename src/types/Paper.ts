68addf251cc9ba32a4f32bf34f27d9e26e38abf3
Isaiah Moore <imoore@udel.edu>
2026-05-23 17:07:36 -0400

    Added a new Paper type called "status" which includes "To Read", "Reading", and "Finished". Also wrote a comment explaining what each type does.

--- src/types/Paper.ts
+++ src/types/Paper.ts
@@ -1,4 +1,4 @@
-
+export type PaperStatus = "To Read" | "Reading" | "Finished";
 
 export interface Paper {
     id: number;
@@ -6,6 +6,17 @@ export interface Paper {
     authors: string[];
     year: number;
     venue: string;
+    status: PaperStatus;
     tags: string[];
     notes: string;
 }
+
+/*
+title: displays the paper's title
+authors: displays who wrote the paper
+year: displays the publication year of the paper.
+venus: displays the journal or conference where the paper was published.
+status: this is so I can filter papers by their reading status (ex: "To Read," "Reading," "Finished"). 
+tags: this is for grouping papers by topic or theme (ex: "Machine Learning," "Data Science," "Computer Vision").
+notes: this is what I will use to "track my research" aka including my thoughts, summaries, and insights about the paper as I read it.
+*/
