1fc219612a6df0ed3a3624e25537395edf11252c
Isaiah Moore <imoore@udel.edu>
2026-05-24 11:25:00 -0400

    Additional change unsaved from last commit.

--- src/components/PaperList.tsx
+++ src/components/PaperList.tsx
@@ -2,3 +2,7 @@ import { Paper } from "../types/Paper";
 import PaperCard from "./PaperCard";
 
 //Task: receive an array of papers and render a PaperCard for each one.
+
+interface PaperListProps {
+  papers: Paper[];
+}
