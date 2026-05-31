f9aa1ba84bcb8d3af6a629f2cc6b86b43e28eab9
Isaiah Moore <imoore@udel.edu>
2026-05-31 00:59:30 -0400

    PaperList.tsx | Wrote out map function.

--- src/components/PaperList.tsx
+++ src/components/PaperList.tsx
@@ -9,8 +9,14 @@ interface PaperListProps {
 
 export function PaperList({papers}: PaperListProps) {
   return (
-
     //For every paper in the papers array, render one PaperCard.
     //Uses Map...
+    <section>
+      <h2>Research Papers</h2>
+      
+    {papers.map((paper) => (
+      <PaperCard key = {paper.id} paper = {paper} />
+    ))}
+    </section>
   );
 }
